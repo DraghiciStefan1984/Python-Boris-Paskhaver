cb212db277ae6ffb7f4415e458481f6db02e337b
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-05-07 09:40:22 +0300

    dicts continued

--- dicts/dicts1.py
+++ dicts/dicts1.py
@@ -6,4 +6,16 @@ nums={'unu':1, 'doi':2, 'trei':3, 'patru':4, 'cinci':5}
 print(nums['unu'])
 
 # if this key does not exist, it will return a default value
-print(nums.get('sase', 6))s
+print(nums.get('sase', 6))
+
+employees={'devs': ['Stefan', 'Bogdan', 'Ana', 'Andrei'],
+            'managers': ['Gabi', 'Victor'],
+            'qa': ['Maria', 'Vlad'],
+            'sales': ['Monica', 'Andrei']}
+
+#adding a key to a dict
+employees['human resources']=['Simona', 'Claudiu']
+print(employees['managers'])
+
+employees['devs'][1]='Vali'
+print(employees['devs'])
