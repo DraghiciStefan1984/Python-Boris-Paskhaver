c925c39f974fc437adefdf42afae3b1bf3373402
draghicistefan <draghicistefan1984@gmail.com>
2020-05-15 10:07:11 +0300

    inheritence done

---
+++ inheritance/composition.py
@@ -0,0 +1,36 @@
+class Paper:
+    def __init__(self, text, case):
+        self.text=text
+        self.case=case
+
+
+class Briefcase:
+    def __init__(self, price):
+        self.price=price
+        self.papers=[]
+
+    def add_paper(self, paper):
+        self.papers.append(paper)
+
+    def view_notes(self):
+        return [paper.text for paper in self.papers]
+
+
+class Lawyer:
+    def __init__(self, name, briefcase):
+        self.name=name
+        self.briefcase=briefcase
+
+    def write_note(self, text, case):
+        paper=Paper(text, case)
+        self.briefcase.add_paper(paper)
+
+    def view_notes(self):
+        print(self.briefcase.view_notes())
+
+
+b=Briefcase(19.99)
+l=Lawyer('Bob', b)
+l.write_note('my client is guilty.', 'aa222')
+l.write_note('he should be punished.', 'ab443')
+l.view_notes()
